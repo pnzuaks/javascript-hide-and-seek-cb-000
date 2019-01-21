f3f14091c6ad15c250f47517009c488752150a92
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-21 21:06:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+
+}
+
+function deepestChild() {
+  var grand = document.getElementById('grand-node')
+  var allDivs = grand.querySelectorAll('div')
+
+  for (let i = 0; i < allDivs.length; i++) {
+    if (allDivs[i].textContent !== null)
+      return allDivs[i]
+  }
+}
